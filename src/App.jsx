7f246acbfacb766d44b918f85ff0c457479ced63
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-19 09:32:54 +0700

    Loop through array object to display list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
+import { vnList } from "./model/data";
+import List from "./list";
 
-function App() {}
+function App() {
+  let displayList = vnList.map((item, index) => {
+    return <List key={index} props={item} />;
+  });
+
+  return <>{displayList}</>;
+}
 
 export default App;
