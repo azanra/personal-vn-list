967972627f84be966568c282a93ac75b1ee9758a
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-21 17:19:45 +0700

    Destructure list to consist only data that needed

---
+++ src/component/detail.jsx
@@ -0,0 +1,15 @@
+export default function Detail({ list }) {
+  let arrList = [];
+  const {
+    romaji,
+    image,
+    id,
+    title,
+    publisher,
+    relation,
+    links,
+    ...detailList
+  } = list;
+
+  return <></>;
+}
