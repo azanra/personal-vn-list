387356523f7a34213ba9797caa41afe25f211322
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-21 17:21:25 +0700

    Add function to create array from object key-value

--- src/component/detail.jsx
+++ src/component/detail.jsx
@@ -1,3 +1,9 @@
+function createArrayFromObject(object, array) {
+  for (let key in object) {
+    array.push(`${key} : ${object[key]}`);
+  }
+}
+
 export default function Detail({ list }) {
   let arrList = [];
   const {
