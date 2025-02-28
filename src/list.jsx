9d9741d04ea17a36633080492f9e6728b9f3cb61
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-28 11:38:29 +0700

    Set the layout, position, and size for header

--- src/list.jsx
+++ src/list.jsx
@@ -23,64 +23,66 @@ export default function List({ list }) {
     setReview(event.target.value);
   }
   return (
-    <>
-      <div className="header-section">
-        <h2>{list.title}</h2>
-        <p>{list.romaji}</p>
+    <div className="p-10">
+      <div className="header">
+        <div className="header-section">
+          <h2 className="text-6xl">{list.title}</h2>
+          <p className="text-xl mt-3">{list.romaji}</p>
+        </div>
+        <div className="image-section ml-150">
+          <img
+            src={list.image}
+            alt={list.alias}
+            style={{
+              width: "300px",
+              borderRadius: "5px",
+            }}
+          />
+        </div>
       </div>
-      <div className="image-section">
-        <img
-          src={list.image}
-          alt={list.alias}
-          style={{
-            width: "300px",
-            borderRadius: "5px",
-          }}
-        />
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            alert("Update Success");
-          }}
-        >
-          <div className="read-status">
-            <label htmlFor="status-input">Read Status : </label>
-            <select
-              name="haveRead"
-              id="status-input"
-              value={haveRead}
-              onChange={handleReadClick}
-            >
-              <option value={true}>Have Read</option>
-              <option value={false}>Not Yet</option>
-            </select>
-          </div>
-          <div className="rating">
-            <label htmlFor="rating-input">Rating: </label>
-            <input
-              type="number"
-              id="rating-input"
-              name="rating"
-              onChange={handleSetRating}
-              value={rating}
-              min={0}
-              max={10}
-            />
-          </div>
-          <div className="review">
-            <label htmlFor="review-input">Review : </label>
-            <textarea
-              name="review"
-              id="review-input"
-              value={review}
-              onChange={handleSetReview}
-            ></textarea>
-          </div>
-          <button type="submit">Update</button>
-        </form>
-      </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          alert("Update Success");
+        }}
+      >
+        <div className="read-status">
+          <label htmlFor="status-input">Read Status : </label>
+          <select
+            name="haveRead"
+            id="status-input"
+            value={haveRead}
+            onChange={handleReadClick}
+          >
+            <option value={true}>Have Read</option>
+            <option value={false}>Not Yet</option>
+          </select>
+        </div>
+        <div className="rating">
+          <label htmlFor="rating-input">Rating: </label>
+          <input
+            type="number"
+            id="rating-input"
+            name="rating"
+            onChange={handleSetRating}
+            value={rating}
+            min={0}
+            max={10}
+          />
+        </div>
+        <div className="review">
+          <label htmlFor="review-input">Review : </label>
+          <textarea
+            name="review"
+            id="review-input"
+            value={review}
+            onChange={handleSetReview}
+          ></textarea>
+        </div>
+        <button type="submit">Update</button>
+      </form>
       <Detail list={list} />
-    </>
+    </div>
   );
 }
 
